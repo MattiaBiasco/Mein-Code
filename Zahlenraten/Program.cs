dafa48cf6c6374341fafb0bc8f86963c2e89fd6c
MattiaBiasco <142606014+MattiaBiasco@users.noreply.github.com>
2023-09-22 16:20:01 +0200

    Add files via upload

---
+++ Zahlenraten/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Zahlenraten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Number = new Random();
+            int randomNumber = Number.Next(1, 100);
+            bool erraten = false;
+            Console.WriteLine("Schreibe eine Zahl zwischen 1 und 100");
+
+            while (!erraten)
+            {
+                
+                int Zahl1 = Convert.ToInt32(Console.ReadLine());
+                if (Zahl1 > randomNumber)
+                {
+                    Console.WriteLine("Tiefer!");
+                    
+                }
+                else if (Zahl1 < randomNumber)
+                {
+                    Console.WriteLine("Höher!");
+
+                }
+                else
+                {
+                    erraten = true;
+                    Console.WriteLine("Zahl erraten!");
+                    
+                    
+                }
+
+                    
+
+            } 
+
+            
+            
+               
+
+
+        }
+    }
+}
